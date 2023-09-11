21963430b52f7c1e9829a8730f428c6680c09258
Fyodor Sizov <fedia.sizov@gmail.com>
2023-09-11 23:24:58 +0300

    produced output with traineddata v3

---
+++ pipelines/nenl1985.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/bash
+git show ynk-nenl1985-v3:training/nenl1985/ynk.traineddata > /usr/share/tesseract-ocr/4.00/tessdata/ynk.traineddata ;
+
+mkdir -p output/nenl1985/ ;
+
+cp input/nenl1985/* output/nenl1985/ ;
+
+python3 scripts/process_pages.py output/nenl1985 ynk ;
+
+python3 scripts/nenl1985_format_pages.py output/nenl1985 ;
+
+rm -f output/nenl1985/*.jpg ;
+rm -f output/nenl1985/*.tiff ;
+rm -f output/nenl1985/*.png ;
