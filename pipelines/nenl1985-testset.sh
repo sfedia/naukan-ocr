556147c053c74f6c833f1127e0c83e8801150cd4
Fyodor Sizov <fedia.sizov@gmail.com>
2023-09-19 12:40:27 +0300

    updated paths

--- pipelines/nenl1985-testset.sh
+++ pipelines/nenl1985-testset.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
-git show ynk-nenl1985-v3:training/nenl1985/ynk.traineddata > /usr/share/tesseract-ocr/4.00/tessdata/ynk.traineddata ;
+git show ynk-nenl1985-v3.1:train/nenl1985/ynk.traineddata > /usr/share/tesseract-ocr/4.00/tessdata/ynk.traineddata ;
 
 cp input/nenl1985-testset/* output/nenl1985-testset/ ;
 
