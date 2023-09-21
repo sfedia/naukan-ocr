b39f62273a49a3c4a235f21715b0a778983bbd11
Fyodor Sizov <fedia.sizov@gmail.com>
2023-09-21 22:12:22 +0300

    added parsed results (nenl1990)

---
+++ pipelines/nenl1990.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/bash
+git show ynk-nenl1990-v1:train/nenl1990/ynk.traineddata > /usr/share/tesseract-ocr/4.00/tessdata/ynk.traineddata ;
+
+mkdir -p output/nenl1990/ ;
+
+cp -r input/nenl1990-splpages/* output/nenl1990;
+
+python3 scripts/process_pages.py output/nenl1990 ynk ;
+
+rm -rf output/nenl1990/*.jpg ;
