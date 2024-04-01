10eb6dc2c0d1e7079be5cfe014cf8264ea521525
Fyodor Sizov <fedia.sizov@gmail.com>
2024-04-01 16:35:03 +0200

    generated boxes for gol2004

--- scripts/gol2004.py
+++ scripts/gol2004.py
@@ -3,11 +3,23 @@ import sys
 import re
 sys.path.append(dirname(abspath(path_join(__file__, ".."))))
 
-from processors.PdfToJpgPages import PdfToJpgPages
+# from processors.PdfToJpgPages import PdfToJpgPages
 
-conv = PdfToJpgPages(
-    "/app/data/gol2004-raw/gol2004.pdf",
-    "/app/data/gol2004-pages",
-    "gol2004"
+# conv = PdfToJpgPages(
+#     "/app/data/gol2004-raw/gol2004.pdf",
+#     "/app/data/gol2004-pages",
+#     "gol2004"
+# )
+# conv.convert()
+
+from processors.ImagesToBoxes import ImagesToBoxes
+from processors.PinModel import PinModel
+
+PinModel("ynk-nenl1985-v3.1", "train/nenl1985/ynk.traineddata", "ynk").pin()
+conv = ImagesToBoxes(
+    "/app/data/gol2004-pages-clean",
+    "/app/data/gol2004-boxes",
+    extension="jpg",
+    img_name_prefix="gol2004"
 )
 conv.convert()
